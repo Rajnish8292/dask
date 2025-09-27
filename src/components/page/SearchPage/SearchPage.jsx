5618fc717462d13035019d9bb0f67a721a88a14f
Rajnish8292 <rajnish81018@gmail.com>
2025-09-27 18:55:58 +0530

    completed recent search feature

--- src/components/page/SearchPage/SearchPage.jsx
+++ src/components/page/SearchPage/SearchPage.jsx
@@ -7,52 +7,37 @@ import ResultSuspense from "@/components/ui/ResultSuspense/ResultSuspense";
 import SearchBox from "@/components/ui/SearchBox/SearchBox";
 import ServerError from "@/components/ui/ServerError/ServerError";
 import StartExample from "@/components/ui/StartExample/StartExample";
-import { useEffect, useRef, useState } from "react";
-import { CiSearch } from "react-icons/ci";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function SearchPage({ example }) {
   const [isSearching, setIsSearching] = useState(false);
   const [isError, setIsError] = useState(false);
   const [result, setResult] = useState(null);
+  const [isRecentSearch, setIsRecentSearch] = useState(false);
 
   const [pasteTextFn, setPasteTextFn] = useState(() => () => {});
 
-  const startWithExampleHandler = ({ pasteText }) => {
+  const startWithExampleHandler = useCallback(({ pasteText }) => {
     setPasteTextFn(() => pasteText);
-  };
+  }, []);
 
-  const shareSearchRequestData = ({ loading, error, result }) => {
+  const shareSearchRequestData = useCallback(({ loading, error, result }) => {
     setIsSearching(loading);
     setIsError(error);
     setResult(result);
-  };
+  }, []);
 
-  // const searchRequest = async ({ query }) => {
-  //   if (query.length == 0) return;
-  //   setIsSearching(true);
-  //   try {
-  //     let request = await fetch("/api/search", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ query: query }),
-  //     });
-  //     let result = await request.json();
-  //     setStatus(200);
-  //     setResult(result);
-  //     console.log({ result });
-  //     setNoOfFacets(
-  //       result.facets.companies.length +
-  //         result.facets.topics.length +
-  //         result.facets.difficulty.length
-  //     );
-  //   } catch (error) {
-  //     setStatus(500);
-  //   }
+  useEffect(() => {
+    const recentSearchData = JSON.parse(
+      localStorage.getItem("recent_searches")
+    );
 
-  //   setIsSearching(false);
-  // };
+    if (recentSearchData && recentSearchData.length > 0) {
+      setIsRecentSearch(true);
+    } else {
+      setIsRecentSearch(false);
+    }
+  }, []);
 
   return (
     <>
@@ -60,43 +45,7 @@ export default function SearchPage({ example }) {
         startWithExampleHandler={startWithExampleHandler}
         shareSearchRequestData={shareSearchRequestData}
       />
-      {/* <ResultAction noOfResult={50} />
-      <Result
-        result={[
-          {
-            title: "Two Sum",
-            difficulty: "Easy",
-            companies: ["Amazon", "Microsoft", "Google"],
-            topics: ["Array", "Hash Table"],
-          },
-          {
-            title: "Longest Substring Without Repeating Characters",
-            difficulty: "Medium",
-            companies: ["Adobe", "Facebook", "Netflix"],
-            topics: ["String", "Sliding Window"],
-          },
-          {
-            title: "Merge k Sorted Lists",
-            difficulty: "Hard",
-            companies: ["Amazon", "Uber", "LinkedIn"],
-            topics: ["Linked List", "Heap", "Divide and Conquer"],
-          },
-          {
-            title: "Binary Tree Level Order Traversal",
-            difficulty: "Medium",
-            companies: ["Google", "Apple", "Microsoft"],
-            topics: ["Tree", "Breadth-First Search"],
-          },
-          {
-            title: "Valid Parentheses",
-            difficulty: "Easy",
-            companies: ["Amazon", "Facebook"],
-            topics: ["String", "Stack"],
-          },
-        ]}
-      /> */}
-
-      <RecentSearch />
+      {isRecentSearch && <RecentSearch />}
       {!isSearching &&
         !isError &&
         !result &&
@@ -118,29 +67,6 @@ export default function SearchPage({ example }) {
           />
         </>
       )}
-      {/* <ServerError /> */}
-      {/* {isSearching && <ResultSuspense />}
-      {status != 200 && <ServerError />} */}
-      {/* {!isSearching && status == 200 && "result" in result && (
-        <>
-          <div className="search_result">
-            <div className="search_found_text">
-              Search result found ({result.result.length})
-            </div>
-            {result.result.map((problem, index) => {
-              return (
-                <Result
-                  title={problem.title}
-                  difficulty={problem.difficulty}
-                  companies={problem.companies}
-                  topics={problem.topics}
-                  key={index}
-                />
-              );
-            })}
-          </div>
-        </>
-      )} */}
     </>
   );
 }
